ce6ced0a5e504c95a8d1e66f0d48aa96b9dc2a32
B0RlS <Dosya0318@gmail.com>
2019-07-14 16:29:34 +0300

    Added Rack-cors

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,13 @@ module TestApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.autoload_paths += %W(#{config.root}/app/services)
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
 
     # Settings in config/environments/* take
     # precedence over those specified here.
