a22896c8e866af5ffbb277c0958b3c82c357e8a0
B0RlS <Dosya0318@gmail.com>
2019-07-15 11:04:07 +0300

    Added equation model validation

--- app/models/equation.rb
+++ app/models/equation.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Equation < ApplicationRecord
+  validates :a_coefficient, presence: true
+  validates :b_coefficient, presence: true
+  validates :c_coefficient, presence: true
 end
